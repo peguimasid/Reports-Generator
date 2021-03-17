1e3636ad1fa5ad159caae6b46f9ffee9d59a9cec
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-17 18:35:00 -0300

    retornando o maior valor

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -7,6 +7,8 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def fetch_higher_cost(report), do: Enum.max_by(report, fn {_k, v} -> v end)
+
   defp sum_values([id, _order, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
